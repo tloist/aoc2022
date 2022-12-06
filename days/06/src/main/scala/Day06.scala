7f5d081b9621853af5c842c60d305fec73bbed8f
Timo Loist <timo.loist@innoq.com>
2022-12-06 10:15:34 +0100

    Day 6: Make it a bit more ideomatic

--- days/06/src/main/scala/Day06.scala
+++ days/06/src/main/scala/Day06.scala
@@ -3,7 +3,7 @@ import better.files.Resource
 def indexOfMarkerEnd(signal: String, markerSize: Int): Option[Int] =
     signal.sliding(markerSize).find(_.toSet.size == markerSize).map(signal.indexOf).map(_ + markerSize)
 
-val input = Resource.asString("inputA.txt").getOrElse(throw new IllegalStateException("Couldn't read input file"))
+val input = Resource.asString("inputA.txt").getOrElse(sys.error("Couldn't read input file"))
 
 @main def part1(): Unit = println(indexOfMarkerEnd(input, 4).map(res => s"The packet starts at '$res'").getOrElse("The packet doesn't start at all!"))
 @main def part2(): Unit = println(indexOfMarkerEnd(input, 14).map(res => s"The message starts at '$res'").getOrElse("The message doesn't start at all!"))
